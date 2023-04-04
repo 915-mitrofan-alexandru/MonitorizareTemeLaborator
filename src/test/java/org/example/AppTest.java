1edfa91ca297f91f6041e276f558b78bba9b47c1
915-mitrofan-alexandru <alexkaragaya@gmail.com>
2023-04-04 22:06:04 +0300

    changed test

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,15 +1,20 @@
 package org.example;
 
+import domain.Student;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import service.Service;
+import validation.StudentValidator;
+import validation.ValidationException;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest
+        extends TestCase
 {
+    private Service service;
     /**
      * Create the test case
      *
@@ -29,10 +34,38 @@ public class AppTest
     }
 
     /**
-     * Rigourous Test :-)
+     * Rigourous Test 🙂
      */
     public void testApp()
     {
         assertTrue( true );
+    }
+
+    public void addGoodStudentTest()
+    {
+        validation.Validator<Student> studentValidator = (validation.Validator<Student>) new StudentValidator();
+        validation.Validator<domain.Tema> temaValidator = (validation.Validator<domain.Tema>) new validation.TemaValidator();
+        validation.Validator<domain.Nota> notaValidator = (validation.Validator<domain.Nota>) new validation.NotaValidator();
+
+        repository.StudentXMLRepository fileRepository1 = new repository.StudentXMLRepository(studentValidator, "studenti.xml");
+        repository.TemaXMLRepository fileRepository2 = new repository.TemaXMLRepository(temaValidator, "teme.xml");
+        repository.NotaXMLRepository fileRepository3 = new repository.NotaXMLRepository(notaValidator, "note.xml");
+
+        service = new Service(fileRepository1, fileRepository2, fileRepository3);
+        assert service.saveStudent("10", "Andrei mocanu", 935) == 0;
+    }
+
+    public void addBadStudentTest()
+    {
+        validation.Validator<Student> studentValidator = (validation.Validator<Student>) new StudentValidator();
+        validation.Validator<domain.Tema> temaValidator = (validation.Validator<domain.Tema>) new validation.TemaValidator();
+        validation.Validator<domain.Nota> notaValidator = (validation.Validator<domain.Nota>) new validation.NotaValidator();
+
+        repository.StudentXMLRepository fileRepository1 = new repository.StudentXMLRepository(studentValidator, "studenti.xml");
+        repository.TemaXMLRepository fileRepository2 = new repository.TemaXMLRepository(temaValidator, "teme.xml");
+        repository.NotaXMLRepository fileRepository3 = new repository.NotaXMLRepository(notaValidator, "note.xml");
+
+        service = new Service(fileRepository1, fileRepository2, fileRepository3);
+        assert service.saveStudent("9", "", 2) == 1;
     }
 }
